639b32befbe33e42e58942f1b09ee798b9c3b33e
Ian Weeks <ianweeks2004@yahoo.com>
2014-03-12 19:59:50 +0000

    Added a static email validation method to the email class. Please validate all email addresses on the server side.

--- src/java/controllers/ProfileServlet.java
+++ src/java/controllers/ProfileServlet.java
@@ -36,15 +36,7 @@ public class ProfileServlet extends HttpServlet
         try (PrintWriter out = response.getWriter())
         {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ProfileServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ProfileServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            System.out.println(request.getParameter("change_email"));
         }
     }
 
