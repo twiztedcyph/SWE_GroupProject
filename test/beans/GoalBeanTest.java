b8b65b7387a08b4df8363e84b3f099a941f0f354
Ian <Twiz@Twiz-PC>
2014-04-01 21:49:17 +0100

    Adding tests 43% total

--- test/beans/GoalBeanTest.java
+++ test/beans/GoalBeanTest.java
@@ -7,6 +7,7 @@
 package beans;
 
 import java.sql.Date;
+import java.sql.SQLException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -46,46 +47,23 @@ public class GoalBeanTest
     }
 
     /**
-     * Test of findByID method, of class GoalBean.
-     */
-    @Test
-    public void testFindByID() throws Exception
-    {
-        System.out.println("findByID");
-        int goalID = 0;
-        GoalBean expResult = null;
-        GoalBean result = GoalBean.findByID(goalID);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of toString method, of class GoalBean.
-     */
-    @Test
-    public void testToString()
-    {
-        System.out.println("toString");
-        GoalBean instance = new GoalBean();
-        String expResult = "";
-        String result = instance.toString();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
      * Test of persist method, of class GoalBean.
      */
     @Test
     public void testPersist() throws Exception
     {
         System.out.println("persist");
-        GoalBean instance = new GoalBean();
-        instance.persist();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        GoalBean instance = new GoalBean(5, false,  
+                    Date.valueOf("2014-04-01"), Date.valueOf("2014-05-01"), 12, 
+                    "gain", "weight");
+        try
+        {
+            instance.persist();
+        } catch (SQLException e)
+        {
+            fail("An exception was thrown.");
+        }
+        assertTrue(true);
     }
 
     /**
@@ -95,12 +73,11 @@ public class GoalBeanTest
     public void testGetSubmitter()
     {
         System.out.println("getSubmitter");
-        GoalBean instance = new GoalBean();
-        int expResult = 0;
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
+        int expResult = 5;
         int result = instance.getSubmitter();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -110,11 +87,11 @@ public class GoalBeanTest
     public void testSetSubmitter()
     {
         System.out.println("setSubmitter");
-        int submitter = 0;
-        GoalBean instance = new GoalBean();
+        int submitter = 7;
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
         instance.setSubmitter(submitter);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(submitter, instance.getSubmitter());
     }
 
     /**
@@ -124,12 +101,11 @@ public class GoalBeanTest
     public void testIsGroupGoal()
     {
         System.out.println("isGroupGoal");
-        GoalBean instance = new GoalBean();
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
         boolean expResult = false;
         boolean result = instance.isGroupGoal();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -139,11 +115,11 @@ public class GoalBeanTest
     public void testSetGroupGoal()
     {
         System.out.println("setGroupGoal");
-        boolean groupGoal = false;
-        GoalBean instance = new GoalBean();
+        boolean groupGoal = true;
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
         instance.setGroupGoal(groupGoal);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(groupGoal, instance.isGroupGoal());
     }
 
     /**
@@ -153,12 +129,11 @@ public class GoalBeanTest
     public void testGetAim()
     {
         System.out.println("getAim");
-        GoalBean instance = new GoalBean();
-        double expResult = 0.0;
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
+        double expResult = 12.0;
         double result = instance.getAim();
         assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -168,11 +143,11 @@ public class GoalBeanTest
     public void testSetAim()
     {
         System.out.println("setAim");
-        double aim = 0.0;
-        GoalBean instance = new GoalBean();
+        double aim = 7.0;
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
         instance.setAim(aim);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(aim, instance.getAim(), 0.0);
     }
 
     /**
@@ -182,12 +157,11 @@ public class GoalBeanTest
     public void testGetCategory()
     {
         System.out.println("getCategory");
-        GoalBean instance = new GoalBean();
-        String expResult = "";
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
+        String expResult = "gain";
         String result = instance.getCategory();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -197,11 +171,11 @@ public class GoalBeanTest
     public void testSetCategory()
     {
         System.out.println("setCategory");
-        String category = "";
-        GoalBean instance = new GoalBean();
+        String category = "lose";
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
         instance.setCategory(category);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(category, instance.getCategory());
     }
 
     /**
@@ -211,12 +185,11 @@ public class GoalBeanTest
     public void testGetType()
     {
         System.out.println("getType");
-        GoalBean instance = new GoalBean();
-        String expResult = "";
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
+        String expResult = "weight";
         String result = instance.getType();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -226,11 +199,11 @@ public class GoalBeanTest
     public void testSetType()
     {
         System.out.println("setType");
-        String type = "";
-        GoalBean instance = new GoalBean();
+        String type = "Body Fat";
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
         instance.setType(type);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(type, instance.getType());
     }
 
     /**
@@ -240,12 +213,11 @@ public class GoalBeanTest
     public void testGetGoalStartDate()
     {
         System.out.println("getGoalStartDate");
-        GoalBean instance = new GoalBean();
-        Date expResult = null;
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
+        Date expResult = Date.valueOf("2014-04-01");
         Date result = instance.getGoalStartDate();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -255,11 +227,11 @@ public class GoalBeanTest
     public void testSetGoalStartDate()
     {
         System.out.println("setGoalStartDate");
-        Date goalStartDate = null;
-        GoalBean instance = new GoalBean();
+        Date goalStartDate = Date.valueOf("2000-07-03");
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
         instance.setGoalStartDate(goalStartDate);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(goalStartDate, instance.getGoalStartDate());
     }
 
     /**
@@ -269,12 +241,11 @@ public class GoalBeanTest
     public void testGetGoalEndDate()
     {
         System.out.println("getGoalEndDate");
-        GoalBean instance = new GoalBean();
-        Date expResult = null;
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
+        Date expResult = Date.valueOf("2014-05-01");
         Date result = instance.getGoalEndDate();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -284,11 +255,11 @@ public class GoalBeanTest
     public void testSetGoalEndDate()
     {
         System.out.println("setGoalEndDate");
-        Date goalEndDate = null;
-        GoalBean instance = new GoalBean();
+        Date goalEndDate = Date.valueOf("2014-06-03");
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
         instance.setGoalEndDate(goalEndDate);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(goalEndDate, instance.getGoalEndDate());
     }
 
     /**
@@ -298,12 +269,11 @@ public class GoalBeanTest
     public void testGetGoalProgress()
     {
         System.out.println("getGoalProgress");
-        GoalBean instance = new GoalBean();
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
         double expResult = 0.0;
         double result = instance.getGoalProgress();
         assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -313,110 +283,10 @@ public class GoalBeanTest
     public void testSetGoalProgress()
     {
         System.out.println("setGoalProgress");
-        double goalProgress = 0.0;
-        GoalBean instance = new GoalBean();
+        double goalProgress = 23.0;
+        GoalBean instance = new GoalBean(5, false, Date.valueOf("2014-04-01"), 
+                Date.valueOf("2014-05-01"), 12, "gain", "weight");
         instance.setGoalProgress(goalProgress);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(goalProgress, instance.getGoalProgress(), 0.0);
     }
-
-    /**
-     * Test of getID method, of class GoalBean.
-     */
-    @Test
-    public void testGetID()
-    {
-        System.out.println("getID");
-        GoalBean instance = new GoalBean();
-        int expResult = 0;
-        int result = instance.getID();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of deleteGoal method, of class GoalBean.
-     */
-    @Test
-    public void testDeleteGoal() throws Exception
-    {
-        System.out.println("deleteGoal");
-        int goalID = 0;
-        int userID = 0;
-        GoalBean.deleteGoal(goalID, userID);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateAim method, of class GoalBean.
-     */
-    @Test
-    public void testUpdateAim() throws Exception
-    {
-        System.out.println("updateAim");
-        int goalID = 0;
-        Double aim = null;
-        GoalBean.updateAim(goalID, aim);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateType method, of class GoalBean.
-     */
-    @Test
-    public void testUpdateType() throws Exception
-    {
-        System.out.println("updateType");
-        int goalID = 0;
-        String type = "";
-        GoalBean.updateType(goalID, type);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateCategory method, of class GoalBean.
-     */
-    @Test
-    public void testUpdateCategory() throws Exception
-    {
-        System.out.println("updateCategory");
-        int goalID = 0;
-        String category = "";
-        GoalBean.updateCategory(goalID, category);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateStartDate method, of class GoalBean.
-     */
-    @Test
-    public void testUpdateStartDate() throws Exception
-    {
-        System.out.println("updateStartDate");
-        int goalID = 0;
-        Date startDate = null;
-        GoalBean.updateStartDate(goalID, startDate);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateEndDate method, of class GoalBean.
-     */
-    @Test
-    public void testUpdateEndDate() throws Exception
-    {
-        System.out.println("updateEndDate");
-        int goalID = 0;
-        Date endDate = null;
-        GoalBean.updateEndDate(goalID, endDate);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
--- test/beans/LifeStyleBeanTest.java
+++ test/beans/LifeStyleBeanTest.java
@@ -55,8 +55,6 @@ public class LifeStyleBeanTest
         String expResult = "";
         String result = instance.getUserOccupation();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
